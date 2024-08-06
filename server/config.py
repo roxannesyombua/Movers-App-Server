a8121f1f93c14fab940cd9f9f198fdf7ed50d30c
Nelson Orina <orinamecha2@gmail.com>
2024-08-06 14:47:20 +0300

    Postgress Implimentation

--- server/config.py
+++ server/config.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 
 # Initialize extensions
 db = SQLAlchemy()
@@ -14,8 +15,9 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+    load_dotenv()
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///app.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
 
--- server/models.py
+++ server/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
 class Inventory(db.Model):
     __tablename__ = "inventory"

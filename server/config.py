63a3cef6d1d3632f31c923c1a7b2a9570981a98a
Mohammedadn <mohammedaden023@gmail.com>
2024-08-07 11:32:57 +0300

    me

--- server/config.py
+++ server/config.py
@@ -11,13 +11,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///app.db')  # Correct default value
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your_jwt_secret_key')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.example.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'flashads9@gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
     MAIL_USE_TLS = bool(os.environ.get('MAIL_USE_TLS', True))
     MAIL_USE_SSL = bool(os.environ.get('MAIL_USE_SSL', False))
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'your_email@example.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_email_password')
-    MAIL_DEFAULT_SENDER = (os.environ.get('MAIL_SENDER_NAME', 'Movers App'), os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@example.com'))
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'flashads9@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '07281534Sa')
+    MAIL_DEFAULT_SENDER = (os.environ.get('MAIL_SENDER_NAME', 'Movers App'), os.environ.get('MAIL_DEFAULT_SENDER', 'flashads9@gmail.com'))
 
 # Initialize extensions
 db = SQLAlchemy()
